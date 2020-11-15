7619d6dde7b64132868741fc6dbc5c3ff2234183
Ting Wang <wt2015-github@users.noreply.github.com>
2020-11-14 19:26:04 -0800

    Update 1_qc.sh

--- 1_qc.sh
+++ 1_qc.sh
@@ -7,13 +7,10 @@
 
 module load FastQC/0.11.9-Java-11
 
-fastq_files1=(`find /fh/fast/grady_w/users/twang23/halberg/exome/wave1/fastq/batch1/ -maxdepth 1 -name "*.fastq.gz" -type f | sort`)
-for file1 in ${fastq_files1[@]}; do fastqc $file1 -t 4 -o /fh/fast/grady_w/users/twang23/halberg/qc/raw/fastqc/batch1/; done
-
-fastq_files2=(`find /fh/fast/grady_w/users/twang23/halberg/exome/wave1/fastq/batch2/ -maxdepth 1 -name "*.fastq.gz" -type f | sort`)
-for file2 in ${fastq_files2[@]}; do fastqc $file2 -t 4 -o /fh/fast/grady_w/users/twang23/halberg/qc/raw/fastqc/batch2/; done
+fastq_files1=(`find <path-to-fastqs> -maxdepth 1 -name "*.fastq.gz" -type f | sort`)
+for file1 in ${fastq_files1[@]}; do fastqc $file1 -t 4 -o <path-to-fastqc-output>; done
 
 module load MultiQC/1.9-foss-2019b-Python-3.7.4
 
-multiqc -v -o /fh/fast/grady_w/users/twang23/halberg/qc/raw/multiqc/ /fh/fast/grady_w/users/twang23/halberg/qc/raw/fastqc/
+multiqc -v -o <path-to-multiqc-output> <path-to-fastqc-output>
 
